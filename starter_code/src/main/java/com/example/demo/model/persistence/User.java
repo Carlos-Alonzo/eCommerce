472ac0637fe7f29401a09ad53d55f7ed82672226
Carlos-Alonzo <alonzo.gby@gmail.com>
2019-11-15 11:08:52 -0500

    Lesson 2 exercise update

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -16,7 +16,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User
+{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,36 +27,34 @@ public class User {
 	@Column(nullable = false, unique = true)
 	@JsonProperty
 	private String username;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
 	
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
     private Cart cart;
-	
+
 	public Cart getCart() {
 		return cart;
 	}
-
 	public void setCart(Cart cart) {
 		this.cart = cart;
 	}
-
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
-	
-	
+	public String getPassword()	{		return password; }
+	public void setPassword(String password)	{		this.password = password; }
 }
