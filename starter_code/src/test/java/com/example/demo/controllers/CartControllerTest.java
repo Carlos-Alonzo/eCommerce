883070f578a6a65a960b639485bc6e9753fdaafd
Carlos-Alonzo <alonzo.gby@gmail.com>
2019-11-20 14:37:27 -0500

    incremental

---
+++ starter_code/src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CartControllerTest
+{
+
+	private CartController cartController;
+	//Mockery
+	private UserController userController= mock(UserController.class);
+	private CreateUserRequest userRequest = mock(CreateUserRequest.class);
+	private UserRepository userRepo = mock(UserRepository.class); //mocking the User repository class
+	private CartRepository cartRepo = mock(CartRepository.class); //mocking the Cart repository class
+	private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+	@Before
+	public void setUp()
+	{
+//		userController = new UserController();
+		cartController = new CartController();
+		//reason for these injections is because these are object required for a userController
+		TestUtils.injectObjects(userRequest, "username", "Carlos");
+		TestUtils.injectObjects(userRequest, "password", "1234");
+		TestUtils.injectObjects(userController, "confirmPassword", "1234");
+		TestUtils.injectObjects(userController, "userRepository", userRepo);
+		TestUtils.injectObjects(userController, "cartRepository", cartRepo);
+		TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+	}
+
+	@Test
+	public void addToCartTest() throws Exception
+	{
+		//stubbing
+//		when(encoder.encode("testPassword")).thenReturn("ThisIsHashed");
+//		CreateUserRequest r = new CreateUserRequest();
+//		r.setUsername("Test");
+//		r.setPassword("testPassword");
+//		r.setConfirmPassword("testPassword");
+
+		final ResponseEntity<User> userResponse = userController.createUser(userRequest);
+
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+
+		User u = response.getBody();
+		assertNotNull(u);
+		assertEquals(0, u.getId());
+		assertEquals("Test", u.getUsername());
+		assertEquals("ThisIsHashed", u.getPassword());
+
+	}
+}
